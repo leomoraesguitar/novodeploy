9b46c422ec936d7ee65294400be6af347876a8e7
leomoraesguitar <123950901+leomoraesguitar@users.noreply.github.com>
2024-09-13 15:43:04 -0300

    ti

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 import flet as ft
 def main(page: ft.Page):
-    page.add(ft.TextField(value = '456', ))
+
+    def SalvarDadosLocais(nome, valor):
+        page.client_storage.set(nome, valor)
+        
+
+    def LerDadosLocais( nome,  default=None):
+        if page.client_storage.contains_key(nome):
+            return page.client_storage.get(nome)
+        else:
+            return default
+
+
+    def Settext(e):
+        valor = e.control.value
+        SalvarDadosLocais('valortexto',valor)
+    
+    ti = LerDadosLocais('valortexto', default='02')
+    page.add(ft.TextField(value = ti, on_change=Settext))
+
 
 if __name__ == '__main__':  
     
