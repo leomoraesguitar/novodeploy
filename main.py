28548ae3ba12bc3d5fc70cd3240920e6fac6372a
leomoraesguitar <123950901+leomoraesguitar@users.noreply.github.com>
2024-09-14 13:22:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,40 +2,33 @@ import flet as ft
 
 def main(page: ft.Page):
     new_key_field = ft.TextField(label="novo Nome", width=200)
-    json_display = ft.Text("Conteúdo atual do arquivo salvo no LocalStorage:")
     result_label = ft.Text(value="", color="green")
 
+
+    def SalvarDadosLocais( nome, valor):
+        page.client_storage.set(nome, valor)
+        
+
+    def LerDadosLocais( nome,  default=None):
+        if page.client_storage.contains_key(nome):
+            return page.client_storage.get(nome)
+        else:
+            return default
     def submit_content(e):
-        # Função para salvar o valor no LocalStorage via JavaScript
-        page.eval(f"localStorage.setItem('key', '{new_key_field.value}');")
         result_label.value = "Valor salvo no LocalStorage!"
+        SalvarDadosLocais('valor', new_key_field.value)
         page.update()
 
-    def carregar_valor_salvo():
-        # Função para carregar o valor do LocalStorage via JavaScript e exibir no Flet
-        page.eval("""
-        const savedValue = localStorage.getItem('key');
-        if (savedValue) {
-            return savedValue;
-        }
-        return null;
-        """, 
-        lambda result: json_display.update(f"Valor salvo: {result}" if result else "Nenhum valor salvo no LocalStorage.")
-        )
 
+    new_key_field.value = LerDadosLocais('valor')
     submit_button = ft.ElevatedButton(text="Salvar no LocalStorage", on_click=submit_content)
 
-    # Adicionando os componentes à página
     page.add(
         ft.Text("Atualizador de valor no LocalStorage", size=20, weight="bold"),
         new_key_field,
         submit_button,
         result_label,
-        json_display
+
     )
-
-    # Carregar valor salvo no LocalStorage quando a página for carregada
-    carregar_valor_salvo()
-
-# Executando o app Flet
-ft.app(target=main)
+if __name__ == '__main__':
+    ft.app(main, view=ft.AppView.WEB_BROWSER)
